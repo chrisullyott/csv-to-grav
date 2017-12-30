591036e1d629d97213dbd8f3e264b77ee3aca820
Chris Ullyott <chris@monkdevelopment.com>
2017-12-29 23:42:59 -0800

    Build headers with YAML#dump

--- src/Post.php
+++ src/Post.php
@@ -21,21 +21,58 @@ class Post
         $this->slug = slugifier\slugify($this->title);
         $this->date = date(self::$dateFormat, strtotime($this->date));
 
+        $categories = self::explodeList($this->category);
+        $this->category = strtolower($categories[0]);
+
+        $tags = self::explodeList($this->tag);
+        $this->tag = array_map('strtolower', $tags);
+
         $converter = new League\HTMLToMarkdown\HtmlConverter();
         $this->markdown = $converter->convert($this->html);
 
         return $this;
     }
 
+    private function getHeaders()
+    {
+        $data = array(
+            'title'    => $this->title,
+            'slug'     => $this->slug,
+            'date'     => $this->date,
+            'author'   => $this->author,
+            'taxonomy' => array(
+                'category' => $this->category,
+                'tag'      => $this->tag
+            ),
+            'metadata' => array(
+                'author'    => $this->author,
+                'generator' => 'Grav',
+                'keywords'  => implode(', ', $this->tag),
+                'og:title'  => $this->title,
+                'og:type'   => 'article'
+            )
+        );
+
+        return Symfony\Component\Yaml\Yaml::dump($data);
+    }
+
     public function getGravFile()
     {
         $file  = "---\n";
-        $file .= "title: {$this->title}\n";
-        $file .= "date: {$this->date}\n";
-        $file .= "author: {$this->author}\n";
+        $file .= "# https://learn.getgrav.org/content\n\n";
+        $file .= trim($this->getHeaders()) . "\n";
         $file .= "---\n\n";
         $file .= "{$this->markdown}";
 
         return $file;
+    }
+
+    private static function explodeList($list, $delimiter = ',')
+    {
+        $items = explode($delimiter, $list);
+        $items = array_map('trim', $items);
+        $items = array_filter($items);
+
+        return array_values($items);
     }
 }
